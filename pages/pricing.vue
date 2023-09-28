1342decba8b9b3edb33ee25baa5789351e371846
boranbatuhan <boranbatuh@gmail.com>
2023-09-29 00:55:25 +0300

    pricing page

--- pages/pricing.vue
+++ pages/pricing.vue
@@ -1,7 +1,30 @@
 <template>
-    <div>
-        <p class="text-4xl font-baloobhai">PRİCİNG</p>
-        <p class="text-4xl font-glassAntiqua">PRİCİNG</p>
+    <div class="min-h-screen border-y-[1rem] bg-green-400 border-green-950 font-baloobhai text-white">
+        <div class="container mx-auto flex items-start justify-center h-fit py-36 gap-4 flex-wrap">
+            <!-- free plan start -->
+            <div class="flex text-xl items-center justify-start flex-col border-2 border-green-950 w-80 shrink-0 hover:bg-green-500 bg-green-300 text-green-950 hover:text-white rounded-lg py-10">
+                <p class="text-4xl">Free Plan</p>
+                <span class="w-10/12 h-1 bg-gradient-to-r from-transparent via-green-950 to-transparent"></span>
+                <p>0$ - Free</p>
+                <p>QR shortcut</p>
+                <p>10 links</p>
+                <p>10 labels</p>
+                <p>No validate</p>
+            </div>
+            <!-- free plan end -->
+            <!-- premium plan start -->
+            <div class="flex text-xl items-center justify-start flex-col border-2 border-green-950 w-80 shrink-0 hover:bg-green-500 bg-green-300 text-green-950 hover:text-white rounded-lg py-10">
+                <p class="text-4xl">Premium Plan</p>
+                <span class="w-10/12 h-1 bg-gradient-to-r from-transparent via-green-950 to-transparent"></span>
+                <p>10$ - Once</p>
+                <p>QR shortcut</p>
+                <p>99 links</p>
+                <p>30+ labels</p>
+                <p>Valid account</p>
+                
+            </div>
+            <!-- premium plan end -->
+        </div>
     </div>
 </template>
 
