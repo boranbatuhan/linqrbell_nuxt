187b92847009988534bf133dd4bfcb42ee1a7ac6
boranbatuhan <boranbatuh@gmail.com>
2023-09-29 00:58:29 +0300

    fix page colors (labels-pricing)

--- pages/labels.vue
+++ pages/labels.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="min-h-screen border-y-[1rem] bg-indigo-400 border-indigo-950 font-baloobhai text-white">
+    <div class="min-h-screen border-y-[1rem] bg-amber-400 border-amber-950 font-baloobhai text-white">
         <div class="container mx-auto mt-32 flex items-center justify-start flex-col gap-7">
             <p class="text-5xl">Supported Platforms</p>
             <ul class="flex flex-wrap gap-4 p-4 items-start justify-start">
                 <li v-for="(p,index) in platforms.getPlatforms" :key="index" class="w-10 h-10 group relative hover:mx-4 mx-0 transition-all">
                     <img draggable="false" v-if="p=='custom'" src="~~/assets/imgs/starwht.png" alt="customlogo" class="w-10 h-10 select-none transition-all group-hover:-translate-y-5">
                     <Icon :icon="`simple-icons:${p}`" class="w-full h-full transition-all group-hover:-translate-y-5" />
-                    <span class="absolute px-3 py-1 bg-indigo-950 rounded-full opacity-0 group-hover:opacity-100 transition-all -bottom-4 left-1/2 -translate-x-1/2">{{ p }}</span>
+                    <span class="absolute px-3 py-1 bg-amber-950 rounded-full opacity-0 group-hover:opacity-100 transition-all -bottom-4 left-1/2 -translate-x-1/2">{{ p }}</span>
                 </li>
             </ul>
         </div>
--- pages/pricing.vue
+++ pages/pricing.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="min-h-screen border-y-[1rem] bg-green-400 border-green-950 font-baloobhai text-white">
+    <div class="min-h-screen border-y-[1rem] bg-emerald-400 border-emerald-950 font-baloobhai text-white">
         <div class="container mx-auto flex items-start justify-center h-fit py-36 gap-4 flex-wrap">
             <!-- free plan start -->
-            <div class="flex text-xl items-center justify-start flex-col border-2 border-green-950 w-80 shrink-0 hover:bg-green-500 bg-green-300 text-green-950 hover:text-white rounded-lg py-10">
+            <div class="flex text-xl items-center justify-start flex-col border-2 border-emerald-950 w-80 shrink-0 hover:bg-emerald-500 bg-emerald-300 text-emerald-950 hover:text-white rounded-lg py-10">
                 <p class="text-4xl">Free Plan</p>
-                <span class="w-10/12 h-1 bg-gradient-to-r from-transparent via-green-950 to-transparent"></span>
+                <span class="w-10/12 h-1 bg-gradient-to-r from-transparent via-emerald-950 to-transparent"></span>
                 <p>0$ - Free</p>
                 <p>QR shortcut</p>
                 <p>10 links</p>
@@ -13,9 +13,9 @@
             </div>
             <!-- free plan end -->
             <!-- premium plan start -->
-            <div class="flex text-xl items-center justify-start flex-col border-2 border-green-950 w-80 shrink-0 hover:bg-green-500 bg-green-300 text-green-950 hover:text-white rounded-lg py-10">
+            <div class="flex text-xl items-center justify-start flex-col border-2 border-emerald-950 w-80 shrink-0 hover:bg-emerald-500 bg-emerald-300 text-emerald-950 hover:text-white rounded-lg py-10">
                 <p class="text-4xl">Premium Plan</p>
-                <span class="w-10/12 h-1 bg-gradient-to-r from-transparent via-green-950 to-transparent"></span>
+                <span class="w-10/12 h-1 bg-gradient-to-r from-transparent via-emerald-950 to-transparent"></span>
                 <p>10$ - Once</p>
                 <p>QR shortcut</p>
                 <p>99 links</p>
